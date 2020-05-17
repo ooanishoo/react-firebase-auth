548187aea17c1789f87e56f3ff3dd9db9aa8f883
ooanishoo <anish.maharjan12341@gmail.com>
2020-05-17 16:04:25 +1000

    Setup firebase config/ install material-ui/core package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Typography } from "@material-ui/core";
+import firebase from "./firebase";
+
+//const auth = firebase.aut();
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant="h5" component="h2">
+        React Firebase Auth
+      </Typography>
     </div>
   );
 }
