3f056c11e0cb4b5f70131d3c348c3ec693b48776
ooanishoo <anish.maharjan12341@gmail.com>
2020-05-27 17:10:35 +1000

    Export popup component from index

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -1,3 +1,4 @@
+export { default as Popup } from "./Popup";
 export { default as SignIn } from "./SignIn";
 export { default as SignUp } from "./SignUp";
 export { default as ForgotPassword } from "./ForgotPassword";
